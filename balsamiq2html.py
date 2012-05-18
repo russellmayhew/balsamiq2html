3336e7da65ebec958369e14401453d09695f38b7
Russell Mayhew <russell.mayhew@gmail.com>
2012-05-18 09:05:09 -0700

    moved to a better dom-parsing structure

--- balsamiq2html.py
+++ balsamiq2html.py
@@ -1,9 +1,10 @@
 import sys
 from StringIO import StringIO
 from xml.dom.minidom import parse
-from xml.parsers.expat import ParserCreate
+from xml.parsers.expat import XMLParserType
 from elementtree.SimpleXMLWriter import XMLWriter
 from functools import partial
+
 
 def create_title(xml_writer, **kwargs):
 	xml_writer.element('h1', kwargs['text'])
@@ -50,6 +51,43 @@ def create_image(xml_writer, **kwargs):
 	xml_writer.element('img', '', attrs)
 
 
+class MyParser(XMLParserType):
+	outer_src_tags = set(["mockup", "controls", "control"])
+
+
+	def __init__(self, XMLWriter):
+		XMLParserType.__init__(self)
+		self.this_control = { "attrs": {} }
+		self.this_src_tag = ""
+		self.element_stack = []
+		self.control_map = {
+			'Title': partial(create_title, XMLWriter),
+			'TabBar': partial(create_tab_bar, XMLWriter),
+			'VerticalTabBar': partial(create_vert_tab_bar, XMLWriter),
+			'Image': partial(create_image, XMLWriter),
+			}
+
+	def StartElementHandler(self, name, attrs):
+		if name not in self.outer_src_tags:
+			self.this_src_tag = name
+
+		if name == "control":
+			self.this_control["method"] = self.control_map[attrs.get('controlTypeID').rsplit("::", 1)[1]]
+			self.this_control["attrs"].update({
+				'width': attrs['w'],
+				'height': attrs['h'],
+				})
+
+	def CharacterDataHanlder(self, name):
+		if name == "control":
+			self.this_control["method"](**self.this_control["attrs"])
+			self.this_control = { "attrs": {} }
+
+	def CharacterDataHanlder(self, data):
+		data = data.strip()
+		if data:
+			self.this_control["attrs"][self.this_src_tag] = data
+
 
 temp = StringIO()
 
@@ -57,47 +95,9 @@ w = XMLWriter(temp)
 html = w.start('html')
 
 
-outer_src_tags = set(["mockup", "controls", "control"])
-
-control_map = {
-	'Title': partial(create_title, w),
-	'TabBar': partial(create_tab_bar, w),
-	'VerticalTabBar': partial(create_vert_tab_bar, w),
-	'Image': partial(create_image, w),
-	}
-
-this_control = { "attrs": {} }
-this_src_tag = ""
-pending_method = None
-
-# 3 handler functions
-def start_element(name, attrs):
-	if name not in outer_src_tags:
-		globals()['this_src_tag'] = name
-
-	if name == "control":
-		globals()['this_control']["method"] = control_map[attrs.get('controlTypeID').rsplit("::", 1)[1]]
-		globals()['this_control']["attrs"].update({
-			'width': attrs['w'],
-			'height': attrs['h'],
-			})
-
-def end_element(name):
-	if name == "control":
-		globals()['this_control']["method"](**globals()['this_control']["attrs"])
-		globals()['this_control'] = { "attrs": {} }
-
-def char_data(data):
-	data = data.strip()
-	if data:
-		globals()['this_control']["attrs"][globals()['this_src_tag']] = data
 
 
-p = ParserCreate()
-
-p.StartElementHandler = start_element
-p.EndElementHandler = end_element
-p.CharacterDataHandler = char_data
+p = MyParser(w)
 
 with open(sys.argv[1], 'rb') as f:
 	p.ParseFile(f)
